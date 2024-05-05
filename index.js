225a64373e6b7025fe072038b448d452c19b4b1b
ankitkumar-s <168958380+ankitkumar-s@users.noreply.github.com>
2024-05-05 14:29:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,90 @@
 const xhr = new XMLHttpRequest();
+
 let urlFileName = document.location.pathname;
 
+
+
 urlFileName = urlFileName.substring(urlFileName.lastIndexOf("/") + 1);
+
 urlFileName = urlFileName.split(".");
+
+
 
 const URL = `${urlFileName[0]}.json`;
 
+function convertPlaylistToEmbed(url) {
+
+    var playlistID = url.split('list=')[1].split('&')[0];
+
+    var embedSrc = 'https://www.youtube.com/embed/videoseries?list=' + playlistID;
+
+    return embedSrc;
+
+}
+
+
+
+
+
+
+
+
+
+
+
 xhr.open("GET", URL);
+
+
 
 const playlistContainer = document.querySelector(".playlist-container");
 
+
+
 xhr.onload = function () {
+
   let chapters = JSON.parse(xhr.responseText);
+
   chapters.forEach((chapter) => {
+
+    console.log(convertPlaylistToEmbed(chapter.ytLink))
+
     const playlistCard = document.createElement("div");
+
     playlistCard.classList.add("playlist-card");
+
     playlistCard.innerHTML = `<iframe
+
     class="playlist"
-    src="${chapter.embedLink}"
+
+    src="${convertPlaylistToEmbed(chapter.ytLink)}"
+
     title="YouTube video player"
+
     frameborder="0"
+
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+
     referrerpolicy="strict-origin-when-cross-origin"
+
     allowfullscreen
+
   ></iframe>
+
   <a
+
     href=${chapter.ytLink}
+
     ><h3>${chapter.name}</h3></a
+
   >`;
 
+
+
     playlistContainer.append(playlistCard);
+
   });
+
 };
+
 xhr.send();
+
